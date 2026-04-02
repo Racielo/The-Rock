8004113b9cf7bbe3953feccf6254be31a634f878
Angel Raciel Benitez Garcia <angelbenitezgarcia336@gmail.com>
2026-04-02 01:29:51 -0600

    Agrego imagenes en lugaer de emojis

--- views/home.php
+++ views/home.php
@@ -15,16 +15,51 @@
         <img src="assets/img/logo.png" class="logo">
     </div>
 
-    <!-- Buscador -->
-    <div class="buscador">
-        <input type="text" placeholder="Todos los productos">
-        <button>🔍</button>
+    <div class="buscador-2">
+
+        <button class="btn-todos">Todos los productos</button>
+
+        <div class="separador"></div>
+
+        <input type="text" placeholder="Buscar...">
+
+        <button class="btn-buscar">🔍</button>
+
     </div>
 
     <!-- Botones -->
     <div class="acciones">
         <button class="btn-login">Iniciar sesión</button>
         <button class="btn-crear">Crear cuenta</button>
+    </div>
+
+</div>
+<!-- SECCIÓN PRINCIPAL -->
+<div class="hero">
+
+    <!-- Título -->
+    <h1 class="titulo-principal">Productos</h1>
+
+    <!-- Categorías -->
+    <div class="categorias">
+
+        <div class="categoria">
+            <div class="icono">
+                <img src="assets/img/pasteles.jpg">
+            </div>
+            <p>Pasteles</p>
+        </div>
+
+        <div class="categoria">
+            <div class="icono">🎂</div>
+            <p>Eventos</p>
+        </div>
+
+        <div class="categoria">
+            <div class="icono">🥐</div>
+            <p>Panadería</p>
+        </div>
+
     </div>
 
 </div>
