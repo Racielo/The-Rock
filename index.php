24e113c90260da4b7d5903fd11d877aae12211b2
Angel Raciel Benitez Garcia <angelbenitezgarcia336@gmail.com>
2026-04-01 21:11:47 -0600

    Agrego usuarios

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-$url = $_GET['url'] ?? 'home';
+require_once "controllers/HomeController.php";
 
-switch($url){
-    case 'home':
-        require_once "controllers/HomeController.php";
-        $c = new Controller();
-        $c->index();
-        break;
-
-    case 'usuarios':
-        require_once "controllers/UsuarioController.php";
-        $c = new UsuarioController();
-        $c->index();
-        break;
-}
+$controller = new Controller();
+$controller->index();
